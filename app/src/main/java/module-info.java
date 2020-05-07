20721139367e87ee1f3a7a3cb01d6b9c51748831
John Chao <johnzhaoy@yahoo.com>
2020-05-07 17:45:45 +0200

    kinda fixed the bugs of app.src.main.java.ensemble.app, but not entirely

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,7 +1,5 @@
-open module samples {
-    exports ensemble.samples.animation.interpolator;
-    exports ensemble.samples.charts.pie.chart;
-    requires javafx.controls;
+open module Ensemble8 {
+        requires javafx.controls;
         requires javafx.fxml;
         requires javafx.graphics;
         requires java.sql;
@@ -9,4 +7,8 @@ open module samples {
         requires java.desktop;
         requires javafx.swing;
         requires javafx.web;
+        requires lucene.queryparser;
+        requires samples;
+        requires lucene.core;
+        requires lucene.grouping;
 }
