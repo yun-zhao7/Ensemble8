db740d820c367d9c11677167acfa77d9e400272c
John Chao <johnzhaoy@yahoo.com>
2020-05-07 16:28:59 +0200

    set the necessary conditions so that it works on Win 10, Java 13, IntelliJ IDEA with Maven

---
+++ samples/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+open module samples {
+        requires javafx.controls;
+        requires javafx.fxml;
+        requires javafx.graphics;
+        requires java.sql;
+        requires javafx.media;
+        requires java.desktop;
+        requires javafx.swing;
+        requires javafx.web;
+}
